671102b02c3c84bc6095c461174866c4043fb03a
Mauritius Bianchi <bianchi.mauritius@googlemail.com>
2022-02-22 11:15:38 +0100

    Update timestamp format for event log

--- P2P_EventLog_Script.sql
+++ P2P_EventLog_Script.sql
@@ -3,26 +3,9 @@ SELECT
        CONCAT(MANDT, EBELN, EBELP) as "CASE ID"/*Create Case ID*/
        , CONCAT(MANDT, BANFN, BNFPO) as "EVENT ID" 
        , "Create Purchase Requisition" as "EVENT"
-       , BADAT as "START EVENT"
-       , BADAT as "END EVENT"
-       
-       	
+       , FROM_UNIXTIME(UNIX_TIMESTAMP (BADAT), "%d-%m-%Y") as "START EVENT"
+       , FROM_UNIXTIME(UNIX_TIMESTAMP (BADAT), "%d-%m-%Y") as "END EVENT"
+    
 FROM eban_purchase_requisition;
 
-/*Step 2*/
-
-
-/*Step 3*/
-
-
-
-/*Step 4*/
-
-
-
-/*Receive Goods*/
-
-
-/*UNION ALL for Event-Log creation*/
-
-
+/*Purchase Order*/
